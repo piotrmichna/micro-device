27c87bae474abd7bf7c7c57573253ffe664f1e42
Piotr Michna <pm@piotrmichna.pl>
2024-02-10 17:17:11 +0100

    begin define Param class

---
+++ params.py
@@ -0,0 +1,14 @@
+class Param:
+
+    def __init__(self, dat: dict = {}):
+        self._pid = 0
+        self._name = ''
+        self._dir_out = None
+        self._sync_s = 0
+        self._uid = 0
+        self._uname = ''
+        self._uform_str = ''
+        self._utyp = ''
+        self._utpy_name = ''
+        self._utyp_id = 0
+        self._val = None
