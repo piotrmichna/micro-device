c5fa0771c5b7dac2090ad7669e77cd797fc2b8bb
Piotr Michna <pm@piotrmichna.pl>
2024-02-10 17:11:36 +0100

    define reading and remove cfg methods

--- devicetools.py
+++ devicetools.py
@@ -1,4 +1,6 @@
 import json
+import os
+
 
 def write_file(filename, dat):
     """
@@ -11,9 +13,46 @@ def write_file(filename, dat):
         Returns:
             (bool): result writing file.
     """
-    if filename and dat and isinstance(dat, (dict,list)):
+    if filename and dat and isinstance(dat, (dict, list)):
         dat = json.dumps(dat, separators=(',', ':'))
         with open(filename, 'w') as f:
             f.write(dat)
         return True
     return False
+
+
+def read_file(filename):
+    """
+    READ DEVICE CONFIGURATION FROM FILE [*.cfg]
+
+        Parameters:
+            filename (str): name of file with configuration json data
+
+        Returns:
+            data (dict): python dictionary or None when data is't load.
+    """
+    try:
+        with open(filename, "r") as f:
+            dat = f.read()
+    except OSError:
+        return None
+    if dat:
+        dat = json.loads(str(dat))
+    return dat
+
+
+def remove_file(filename):
+    """
+    REMOVE DEVICE CONFIGURATION FILE [*.cfg]
+
+        Parameters:
+            filename (str): name of configuration file to remove.
+
+        Returns:
+            (bool): result deleting file.
+    """
+    try:
+        os.remove(filename)
+    except OSError:
+        return False
+    return True
